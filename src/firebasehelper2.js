639aeaec42bd8ab32e3105cb5327f2b78aba10ea
mazizulak <mazula95@gmail.com>
2017-03-19 00:48:52 +0300

    add and edit functions written for members page as offline simulation

--- src/firebasehelper2.js
+++ src/firebasehelper2.js
@@ -7,7 +7,8 @@ var config = {
 	 };
 	firebase.initializeApp(config);
 	var FIRRef = firebase.database().ref();
-
+var table;
+var globalCell;
 if(document.getElementById("member")==null){
 	console.log("member is null");
 	//nothing
@@ -15,7 +16,7 @@ if(document.getElementById("member")==null){
 }else{
 	console.log("else girdim member");
 	var Members = FIRRef.child('Users');
-		var table = document.getElementById("member").getElementsByTagName('tbody')[0];
+	table = document.getElementById("member").getElementsByTagName('tbody')[0];
 	Members.on('value',function(snapshot){
 	  snapshot.forEach(function(childSnapshot) {
 	//  console.log(childSnapshot.val().name)
@@ -32,7 +33,7 @@ if(document.getElementById("member")==null){
 	  cell1.appendChild(img);
 	  cell2.innerHTML = childSnapshot.val().name;
 	  cell3.innerHTML = childSnapshot.val().voted;
-	  cell4.innerHTML = '<paper-button raised class="red"><i class="material-icons">create</i></paper-button>';
+	  cell4.innerHTML = '<paper-button onclick="editMember('+table.rows.length+');" raised class="red"><i class="material-icons">create</i></paper-button>';
 	  })
 	});
 }
@@ -40,7 +41,36 @@ if(document.getElementById("member")==null){
 function addMember(){
 	var userName = document.getElementById("newUserName");
 	var userMail = document.getElementById("newUserMail");
+	//table = document.getElementById("member").getElementsByTagName('tbody')[0];
+	var row = table.insertRow(table.rows.length);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	var img = document.createElement('img');
+	img.style.height = '65px';
+	img.style.width = '65px';
+	img.src='../images/defaultProfileImage.png';
+	cell1.appendChild(img);
+	cell2.innerHTML = userName.value;
+	cell3.innerHTML = '0';
+	cell4.innerHTML = '<paper-button onclick="editMember('+table.rows.length+');" raised class="red"><i class="material-icons">create</i></paper-button>';
 	console.log(userMail.value+"   "+userName.value);
 	userName.value = "";
 	userMail.value = "";
+}
+
+function editMember(cell){
+	var name = table.rows[cell-1].cells[1].innerHTML;
+	globalCell = cell-1;
+	console.log(table.rows[cell-1].cells[1].innerHTML);
+	dialogForEdit.open();
+	var userName = document.getElementById("editUserName");
+	//var userMail = document.getElementById("newUserMail"); // Firebaseden çekilecek
+	userName.value = name;
+	//userMail.value = "";
+}
+function saveMember(){
+	table.rows[globalCell].cells[1].innerHTML = document.getElementById("editUserName").value;
+	//Mail bilgisi de firebase e buradan gönderilecek
 }
