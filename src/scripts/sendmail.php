35dc4af1e28446fc435e7a6416a1eb7d4b1e7b9b
mazizulak <mazula95@gmail.com>
2017-03-30 11:38:45 +0300

    php function fixed

--- src/scripts/sendmail.php
+++ src/scripts/sendmail.php
@@ -3,16 +3,16 @@ $data = json_decode(file_get_contents('https://project2-hw1.firebaseio.com/Users
 print_r($data);
 echo $data[1];
 
-    /*
-	$to      = 'nobody@example.com';
-	$subject = 'the subject';
-	$message = 'hello';
-	$headers = 'From: webmaster@example.com' . "\r\n" .
-	    'Reply-To: webmaster@example.com' . "\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+	foreach ($data as $email => $value) {
+		$to      = value;
+		$subject = 'You Should Go That Restaurant';
+		$message = 'Hello, please visit the project2-hw1.firebaseapp.com website to see the today \'s new restaurant';
+		$headers = 'From: ulak@itu.edu.tr' . "\r\n" .
+		    'Reply-To: ulak@itu.edu.tr' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
 
-	mail($to, $subject, $message, $headers);
-	*/
+		mail($to, $subject, $message, $headers);
+	}
 
 
 ?>
